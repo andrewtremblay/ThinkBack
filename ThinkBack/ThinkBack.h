d4e9423518a7672d68d08925a4ec4e31586524c7
Andrew Tremblay <gloriousrocketship@gmail.com>
2013-12-19 18:13:24 -0800

    Cleaning up the new ThinkBack object

--- ThinkBack/ThinkBack.h
+++ ThinkBack/ThinkBack.h
@@ -16,29 +16,27 @@ static NSString* kIdeaLogEntityName = @"IdeaLogEntity";
 
 @interface ThinkBack : NSObject
 
+#pragma - Database helpers
 +(NSManagedObjectContext *) getManagedObjectContext;
-
-/*
- Set up the context for testing
- */
 +(void)prepareDataModelForTesting;
+
 +(void)populateDebugDataModel;
 
+#pragma mark primary Idea behavior (common CRUD)
 
-#pragma mark - primary Idea behavior (common CRUD)
 +(ThinkBackIdeaDataObject *)createIdea; //creates a new predicate with default values and adds it to the database. Returns a reference to that managed object.
++(NSArray *)getAllIdeas; //returns all ideas in the database (potentially huge)
 +(NSError *)saveIdea:(ThinkBackIdeaDataObject *)ideaToSave;
 +(NSError *)deleteIdea:(ThinkBackIdeaDataObject *)ideaToDelete; //takes an existing idea and atempts to remove it from the database. Returns true on successful deletion
 
+#pragma mark - Data logic
 +(BOOL)ideaObjectIsValid:(ThinkBackIdeaDataObject *)ideaToCheck;
-//helper setters and getters
+
+#pragma mark - helper setters and getters
 +(void)setRemindType:(ThinkBackRemindType)remindTypeInt forIdea:(ThinkBackIdeaDataObject *)idea;
 +(ThinkBackRemindType)getRemindTypeForIdea:(ThinkBackIdeaDataObject *)idea;
 
-+(NSArray *)getAllIdeas;
-
 #pragma mark String Formatting
 +(NSString *) formattedRemindAtTimeForIdea:(ThinkBackIdeaDataObject *)object;
-
 
 @end
